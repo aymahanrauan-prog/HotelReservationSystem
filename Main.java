9a2e2080d7c51229e86026cb2230c312ad931a2c
aymahanrauan-prog <aymahanrauan@gmail.com>
2025-12-20 09:48:13 +0500

    Refactor booking display logic in Main.java
    Removed room comparison and price check logic.

--- Main.java
+++ Main.java
@@ -5,23 +5,16 @@ public class Main {
         Room room2 = new Room(102, "Double", 20000, true);
 
         Guest guest1 = new Guest("Rauan", "+77001234567");
+        Guest guest2 = new Guest("Islam", "+77078394924");
+        Guest guest3 = new Guest("Ernar", "+77470948490");
 
         Booking booking1 = new Booking(room1, guest1, 3);
+        Booking booking2 = new Booking(room2, guest2, 5);
+        Booking booking3 = new Booking(room2, guest3, 5);
 
         System.out.println("=== Информация о бронировании ===");
         booking1.displayBookingInfo();
-
-        System.out.println("\n=== Сравнение номеров ===");
-        if (room1.equals(room2)) {
-            System.out.println("Номера одинаковые");
-        } else {
-            System.out.println("Номера разные");
-        }
-
-        if (room1.getPrice() > room2.getPrice()) {
-            System.out.println("Комната 101 дороже");
-        } else {
-            System.out.println("Комната 102 дороже");
-        }
+        booking2.displayBookingInfo();
+        booking3.displayBookingInfo();
     }
 }
